86b00a62abf83e42e2a0d890707158d09e5eedcc
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-03-19 00:02:39 +0530

    currency converter

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency)
+{
+    const [data,setData]=useState({});
+    useEffect(()=>{
+        fetch(`https://currency-rate-exchange-api.onrender.com/${currency}`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res.rates[currency]));
+        // console.log(data);
+    },[currency])
+    // console.log(data);
+    return data;
+}
+export default useCurrencyInfo;
+// https://api.freecurrencyapi.com/v1/latest?apikey=YOUR-APIKEY
